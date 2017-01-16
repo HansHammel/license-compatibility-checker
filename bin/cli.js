8a73db539a6fc9cf7006da738e945f987711883b
HansHammel <oh.schitt@freenet.de>
2017-01-16 21:13:49 +0100

    update notifier added

--- bin/cli.js
+++ bin/cli.js
@@ -1,18 +1,31 @@
 #!/usr/bin/env node
+
 var path=require('path');
+var updateNotifier = require('update-notifier');
+var pkg = require('../package.json');
 var lcc = require('../lib');
+
 if (!module.parent) {
   lcc.check(process.argv[2] || path.join(process.cwd(),'package.json'), process.argv[3] || path.join(process.cwd(),"node_modules"), function(err,passed,output){
 	  if (err) { 
 	    console.log(err); 
-		process.exit(1)
+		updateNotifier({
+			pkg
+		}).notify();
+		process.exit(1);
 	  }
 	  else if (passed)
 	  {
 		console.log(output);
+	  	updateNotifier({
+			pkg
+		}).notify();		
 	  } else
 	  {
 	    console.log(output);
+	  	updateNotifier({
+			pkg
+		}).notify();		
 	  	process.exit(1); 
       }
   }); 
